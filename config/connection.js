9c98d50df60dba63ef10c8a8b7d97597092732c4
Andrew Cawood <cawooda@gmail.com>
2024-05-13 12:53:46 +1000

    config added

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+console.log(__dirname);
+const DIRECTORY = __dirname;
+
+const pool = new Pool(
+    {
+      user: process.env.DB_USERNAME,
+      // TODO: Enter PostgreSQL password
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE
+    },
+    console.log(`Connected to the books_db database.`)
+  )
+
+
+
+module.exports = {pool,DIRECTORY};
